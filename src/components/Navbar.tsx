efabae81ae7755deb7b76a5a7333a33e281f5d4c
Asta1984 <salilmandal908@gmail.com>
2024-12-10 20:13:42 +0530

    metadata

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -54,11 +54,11 @@ export default function Navbar() {
       }`}
     >
       <div className="container mx-auto px-6 py-4 flex justify-between items-center">
-        <Link to="/" className="text-3xl font-semibold flex items-center gap-2">
+        <Link to="/" className="text-xl font-semibold flex items-center gap-2">
           <img 
             src="https://pub-d02e3aa7d09f4d5d9261e5d7e4bae228.r2.dev/logo.svg" 
             alt="Logo" 
-            className='h-10 w-10'
+            className='h-7 w-7'
           />
           SEWANTIKA
         </Link>
